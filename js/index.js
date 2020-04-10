cf2832de670e06d565aa98aa5e28dd8a2964ec4a
Lindsay W <lwhite5314@gmail.com>
2020-04-09 21:36:35 -0400

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+let navLinks = document.querySelector('.intro');
+navLinks.addEventListener('mouseenter',(event)=>{
+    event.target.style.backgroundColor = 'pink';
+    
+});
+
+let body = document.querySelector('body');
+body.addEventListener('onscroll', (event)=>{
+    event.target.style.backgroundColor = 'lightgreen';
+});
+
+
+
+
